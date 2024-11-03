56770a4da7d99528e9d40bc489ab88651f77cd5e
Aman Yadav <Anku-yadav-001>
2024-11-03 09:40:13 +0530

    new chnages

--- api/src/test/java/com/yaduvanshi_brothers/api/ApiApplicationTests.java
+++ api/src/test/java/com/yaduvanshi_brothers/api/ApiApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = ApiApplication.class)
-@ActiveProfiles("prod")
+@ActiveProfiles("dev")
 class ApiApplicationTests {
 
 	@Test
