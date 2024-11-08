b277edbcd5fbaee369552a47de5719dbcf2e2f66
Aman Yadav <Anku-yadav-001>
2024-11-08 22:31:35 +0530

    new chnages

--- ui/src/middleware.js
+++ ui/src/middleware.js
@@ -1,17 +1,12 @@
 import { NextResponse } from 'next/server';
-import Cookies from "js-cookie"
 export function middleware(request) {
     const token = request.cookies.get('username')?.value;  // Access the JWT token
-    const cookies = new Cookies();
-    const newUsername = cookies.get('username')?.value;
-    
-    console.log("Token from middleware: new ", token);  // Debug to ensure token is available
-    console.log("newUsername from middleware: new ", newUsername);  // Debug to ensure token is available
-    if ((!token && !newUsername) && (request.nextUrl.pathname === '/' || request.nextUrl.pathname === '/dashboard')) {
+    console.log("token from middleware:", token);  // Debug to ensure token is available
+    if ((!token) && (request.nextUrl.pathname === '/' || request.nextUrl.pathname === '/dashboard')) {
         return NextResponse.redirect(new URL('/login', request.url));
     }
 
-    if ((token || newUsername) && request.nextUrl.pathname === '/') {
+    if ((token) && request.nextUrl.pathname === '/') {
         return NextResponse.redirect(new URL('/dashboard', request.url));
     }
 
