cbc2b271347a06d99270b0fc9a246491aee813c1
Aman Yadav <Anku-yadav-001>
2024-11-09 10:09:13 +0530

    new chnages

--- ui/src/middleware.js
+++ ui/src/middleware.js
@@ -4,6 +4,9 @@ export function middleware(request) {
     const token = request.cookies.get("username")?.value;
 
     console.log("token from middleware:", token);
+    console.log("request.nextUrl.pathname from middleware:", request.nextUrl.pathname);
+    console.log("request.url from middleware:", request.url);
+    console.log("request from middleware:", request);
     if (!token && request.nextUrl.pathname === '/') {
         return NextResponse.redirect(new URL('/login', request.url));
     }
